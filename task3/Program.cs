4415abc9ecd076597e93ec9ef550c8920db3f990
«anastasiatischenko» <1510anastasia@mail.ru>
2023-02-08 14:39:52 +0300

    Добавили task3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1,4
+
+Console.Clear();
+Console.Write("Введите число: ");
+int userInput = int.Parse(Console.ReadLine()!)!;
+int counter = 1;
+while (counter < userInput)
+{
+    Console.Write(counter * counter + ", ");
+    counter++;
+}
+Console.WriteLine(counter * counter + ".");
